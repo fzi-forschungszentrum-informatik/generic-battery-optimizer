e42d2c3b7537d6b05e6892df7ec9c9cdb23e7392
Carl Hauschke <info@carlhauschke.de>
2024-11-27 15:05:33 +0100

    logging text

--- src/battery_optimizer/static/heat_pump.py
+++ src/battery_optimizer/static/heat_pump.py
@@ -3,3 +3,8 @@ C_TO_K = 273.15
 TES_BLOCK_TEMP_WITH_WARM_WATER_DEMAND = 323.15
 TEXT_INVERTER_ENERGY_RULE = "Inverter Energy"
 TEXT_HEATING_ELEMENT_ENERGY_RULE = "Heating Element Energy"
+
+LOG_ERROR_TEMPERATURE_TOO_HIGH = (
+    "This seems unreasonably high. "
+    "Did you forget to convert from K or °F to °C?"
+)
