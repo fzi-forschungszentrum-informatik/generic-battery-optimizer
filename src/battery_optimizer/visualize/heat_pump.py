cbf879d16834190db08b1662ff2240d5061f1ee6
Carl Hauschke <info@carlhauschke.de>
2024-11-28 11:25:51 +0100

    plot tank soc

--- src/battery_optimizer/visualize/heat_pump.py
+++ src/battery_optimizer/visualize/heat_pump.py
@@ -3,7 +3,48 @@ from battery_optimizer.export.heat_pump import (
     binary_values,
     parameters,
     heat_pump_cop,
-    electric_energy_usage,
     tes_temperature,
     heat_energy_usage,
 )
+import pyomo.environ as pyo
+import matplotlib.pyplot as plt
+
+from battery_optimizer.export.model import to_heat_pump_power
+from battery_optimizer.static.numbers import MAX_COP
+
+
+def plot_tank_soc(heat_pump: str, model: pyo.ConcreteModel, figsize=(10, 6)):
+    # Get the TES temperature data
+    soc = tank_soc(heat_pump, model)
+
+    # Create the plot
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(soc.index, soc.values, marker="o")
+
+    # Format the x-axis labels
+    ax.set_xticks(soc.index)
+    ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter("%H:%M"))
+    ax.xaxis.set_minor_formatter(
+        plt.matplotlib.dates.DateFormatter("%d-%m %H:%M")
+    )
+    ax.xaxis.set_minor_locator(plt.matplotlib.dates.HourLocator(interval=1))
+    plt.xticks(rotation=45)
+
+    # Set the y-axis range and labels
+    ax.set_ylim(0, 1)
+    ax.set_ylabel("SoC")
+    ax.set_yticks(
+        ticks=[i / 10 for i in range(0, 11)],
+        labels=[f"{i*10}%" for i in range(0, 11)],
+    )
+
+    # Add title and labels
+    ax.set_title("TES SoC")
+    ax.set_xlabel("Time")
+
+    # Show the plot
+    ax.grid(True)
+    plt.tight_layout()
+
+    return fig
+
