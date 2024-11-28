14b992737841d095d1ad65c335d88e10eadf03cf
Carl Hauschke <info@carlhauschke.de>
2024-11-28 11:26:05 +0100

    plot heat pump cop

--- src/battery_optimizer/visualize/heat_pump.py
+++ src/battery_optimizer/visualize/heat_pump.py
@@ -48,3 +48,37 @@ def plot_tank_soc(heat_pump: str, model: pyo.ConcreteModel, figsize=(10, 6)):
 
     return fig
 
+def plot_heat_pump_cop(
+    heat_pump: str, model: pyo.ConcreteModel, figsize=(10, 6)
+):
+    # Get the TES temperature data
+    cop = heat_pump_cop(heat_pump, model)
+
+    # Create the plot
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(cop.index, cop.values, marker="o")
+
+    # Format the x-axis labels
+    ax.set_xticks(cop.index)
+    ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter("%H:%M"))
+    ax.xaxis.set_minor_formatter(
+        plt.matplotlib.dates.DateFormatter("%d-%m %H:%M")
+    )
+    ax.xaxis.set_minor_locator(plt.matplotlib.dates.HourLocator(interval=1))
+    plt.xticks(rotation=45)
+
+    # Set the y-axis range and labels
+    ax.set_ylim(0, MAX_COP)
+    ax.set_ylabel("CoP")
+    ax.set_yticks(range(0, 11))
+
+    # Add title and labels
+    ax.set_title("Heat Pump CoP")
+    ax.set_xlabel("Time")
+
+    # Show the plot
+    ax.grid(True)
+    plt.tight_layout()
+
+    return fig
+
