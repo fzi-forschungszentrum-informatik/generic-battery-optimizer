cf54b7ad83767108f4c6a0911b7558f7beae6a69
Carl Hauschke <info@carlhauschke.de>
2025-01-10 10:41:23 +0100

    ensure minimum demand enforces activation of hp

--- src/battery_optimizer/helpers/heat_pump_block.py
+++ src/battery_optimizer/helpers/heat_pump_block.py
@@ -331,10 +331,12 @@ def heat_pump_block_rule(
     block.charge_cons = pyo.Constraint(rule=hp_charge_rule)
 
     def hp_charge_MIND_rule(block):
-        return (
-            block.y_TES * (heat_pump.mind_heat_supply_hp)
-            <= block.heat_supply_hp_tes
+        min_heat_supply_hp = (
+            heat_pump.min_heat_supply_hp
+            if heat_pump.min_heat_supply_hp > 0
+            else 0.00000001
         )
+        return block.y_TES * (min_heat_supply_hp) <= block.heat_supply_hp_tes
 
     block.charge_MIND_cons = pyo.Constraint(rule=hp_charge_MIND_rule)
 
