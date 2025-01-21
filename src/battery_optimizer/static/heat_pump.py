ee611b0981efead70f946690a18fa3299871127e
Carl Hauschke <info@carlhauschke.de>
2025-01-21 13:40:55 +0100

    fix spelling for model output parser

--- src/battery_optimizer/static/heat_pump.py
+++ src/battery_optimizer/static/heat_pump.py
@@ -1,8 +1,8 @@
 TEXT_HEAT_PUMP_BASE = "Heat pump: "
 C_TO_K = 273.15
 TES_BLOCK_TEMP_WITH_WARM_WATER_DEMAND = 323.15
-TEXT_INVERTER_ENERGY_RULE = "Inverter Energy"
-TEXT_HEATING_ELEMENT_ENERGY_RULE = "Heating Element Energy"
+TEXT_INVERTER_ENERGY_RULE = "inverter energy"
+TEXT_HEATING_ELEMENT_ENERGY_RULE = "heating element energy"
 
 LOG_ERROR_TEMPERATURE_TOO_HIGH = (
     "This seems unreasonably high. "
