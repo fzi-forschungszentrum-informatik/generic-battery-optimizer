915fad41c38b7fa2e447de438a5e48097f71af04
Carl Hauschke <info@carlhauschke.de>
2025-04-14 12:17:53 +0100

    fix variable naming

--- src/battery_optimizer/helpers/blocks.py
+++ src/battery_optimizer/helpers/blocks.py
@@ -1,14 +1,15 @@
 import pandas as pd
+from pyomo.core.base.set import OrderedScalarSet
 
 
-def get_period_length(period: pd.Timestamp, index: pd.DatetimeIndex):
+def get_period_length(period: pd.Timestamp, index: OrderedScalarSet):
     """Gets duration of models period
 
     Arguments:
     ----------
         period: pd.Timestamp
             The period to get the duration of
-        index: pd.DatetimeIndex
+        index: OrderedScalarSet
             The index of the model
     Returns:
     --------
@@ -24,4 +25,3 @@ def get_period_length(period: pd.Timestamp, index: pd.DatetimeIndex):
         period_length = index.next(period) - period
         period_conversion_factor = period_length.total_seconds() / 3600
     return period_length, period_conversion_factor
-
