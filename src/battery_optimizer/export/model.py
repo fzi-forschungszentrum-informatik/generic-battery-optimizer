d9be1da6449f1ea72eef784f2c6cb5539c9f7cbe
Carl Hauschke <info@carlhauschke.de>
2025-08-25 19:19:15 -0500

    fix spelling

--- src/battery_optimizer/export/model.py
+++ src/battery_optimizer/export/model.py
@@ -16,7 +16,7 @@ POWER_POSTFIX = TEXT_ENERGY.replace("energy", "power")
 
 
 # Könnte man den jetzt von einem DF erben lassen
-class ModelDataframe:
+class ModelDataFrame:
     """Dataframe with all data from a model"""
 
     def __init__(
@@ -58,7 +58,7 @@ class ModelDataframe:
                 ].items()
             }
         )
-        return ModelDataframe.__convert_to_power(buy_df)
+        return ModelDataFrame.__convert_to_power(buy_df)
 
     def to_sell(self) -> pd.DataFrame:
         """Create a DataFrame with all sell power profiles
@@ -82,7 +82,7 @@ class ModelDataframe:
                 ].items()
             }
         )
-        return ModelDataframe.__convert_to_power(sell_df)
+        return ModelDataFrame.__convert_to_power(sell_df)
 
     def to_battery_power(self) -> pd.DataFrame:
         """Create a DataFrame with all battery power profiles
@@ -101,7 +101,7 @@ class ModelDataframe:
             The power in W of each battery.
         """
         # Get Battery power
-        return ModelDataframe.__convert_to_power(
+        return ModelDataFrame.__convert_to_power(
             pd.DataFrame(
                 {
                     device: pd.Series(values["sink"])
@@ -134,7 +134,7 @@ class ModelDataframe:
                 ].items()
             }
         )
-        return ModelDataframe.__convert_to_power(fixed_consumption_df)
+        return ModelDataFrame.__convert_to_power(fixed_consumption_df)
 
     def to_heat_pump_power(self) -> pd.DataFrame:
         """Create a DataFrame with all heat pump power profiles
@@ -151,7 +151,7 @@ class ModelDataframe:
             The power in W of each heat pump profile.
         """
         # Get Battery power
-        return ModelDataframe.__convert_to_power(
+        return ModelDataFrame.__convert_to_power(
             pd.DataFrame(
                 {
                     device: values["sink"]
@@ -304,7 +304,7 @@ class Exporter:
             }
         return variables
 
-    def to_df(self) -> ModelDataframe:
+    def to_df(self) -> ModelDataFrame:
         """Create a DataFrame from the model
 
         Contains all devices that draw power as columns. Each value represents
@@ -314,7 +314,7 @@ class Exporter:
 
         Returns
         ---------
-            ModelDataframe
+            ModelDataFrame
             A DataFrame-like object containing power consumption data for all
             devices, indexed by timestamps. The DataFrame includes energy
             metrics and battery state of charge (SOC) information.
@@ -357,7 +357,7 @@ class Exporter:
                 for index in component.soc
             }
 
-        return ModelDataframe(variables, soc)
+        return ModelDataFrame(variables, soc)
 
     def write_excel(self, filename: str) -> None:
         """Create an Excel file from the model
