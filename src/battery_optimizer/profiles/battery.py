63ee23bccb0acf6dcdf200c38034dafdc0a20175
Carl Hauschke <hauschke@fzi.de>
2025-10-21 17:06:06 +0200

    Update documentation for battery profile

--- src/battery_optimizer/profiles/battery.py
+++ src/battery_optimizer/profiles/battery.py
@@ -1,10 +1,17 @@
-from pydantic import field_validator, BaseModel, Field
-from battery_optimizer.static.numbers import SECRET_LENGTH
+"""
+Defines a battery model for domestic/car batteries.
+
+This model combines all relevant parameters for battery operation
+in a single data structure.
+"""
+
 from datetime import datetime
 from typing import Optional
 from math import isnan
 import secrets
 import logging
+from pydantic import field_validator, BaseModel, Field
+from battery_optimizer.static.numbers import SECRET_LENGTH
 
 log = logging.getLogger(__name__)
 
@@ -14,65 +21,78 @@ class Battery(BaseModel):
     Stores all information about a domestic/car battery.
 
     Power and energy are assumed to be specified in W and Wh respectively.
-
-    Parameters
-    ----------
-    name: str
-        The name of the battery to reference it in the model.
-        If not supplied it will be populated by a random alphanumerical string
-    start_soc: float
-        The initial SoC of the battery in percent (0-1).
-    end_soc: float
-        The SoC in percent (0-1) that shall be reached by the time end_soc_time is
-        reached.
-        After end_soc_time the battery is not allowed to be discharged below
-        end_soc.
-        This value is optional.
-    end_soc_time: datetime
-        The datetime that specifies the time when end_soc should be reached.
-        This is optional but if it is supplied end_soc must be supplied too.
-    capacity: float
-        The capacity of the battery in Wh
-    max_charge_power: float
-        The maximum power the battery can be charged with in W.
-    max_discharge_power: float
-        The maximum power the battery can be discharged with in W.
-        This is optional. If it isn't supplied discharging the battery is not
-        allowed.
-    charge_efficiency: float
-        Efficiency of the charging process in percent.
-    discharge_efficiency: float
-        Efficiency of the discharge process in percent.
-    min_soc: float
-        Constraint the usable SoC range of the battery.
-        Value is given in percent.
-        This value is optional.
-    max_soc: float
-        Constraint the usable SoC range of the battery.
-        Value is given in percent.
-        This value is optional.
     """
 
     # The batteries name used in the model. This must be unique and is
     # generated automatically
-    name: str = secrets.token_hex(SECRET_LENGTH)
+    name: str = Field(
+        default=secrets.token_hex(SECRET_LENGTH),
+        description=(
+            "The name of the battery to reference it in the model. If not "
+            "supplied it will be populated by a random alphanumerical string."
+        ),
+    )
     # SoCs
-    start_soc: float = Field(ge=0, le=1)
-    end_soc: Optional[float] = Field(None, ge=0, le=1)
+    start_soc: float = Field(
+        ge=0,
+        le=1,
+        title="Initial State of Charge",
+        description="The initial SoC of the battery in percent (0-1).",
+    )
+    end_soc: Optional[float] = Field(
+        None,
+        ge=0,
+        le=1,
+        title="SoC at end of optimization period",
+        description=(
+            "The SoC in percent (0-1) that shall be reached by the time "
+            "end_soc_time is reached. After end_soc_time the battery is not "
+            "allowed to be discharged below end_soc. This value is optional."
+        ),
+    )
 
     # Charge end time
-    end_soc_time: Optional[datetime] = None
+    end_soc_time: Optional[datetime] = Field(
+        default=None,
+        title="End time for reaching end_soc",
+        description=(
+            "The datetime that specifies the time when end_soc should be "
+            "reached. This is optional but if it is supplied end_soc must be "
+            "supplied too."
+        ),
+    )
 
     # Charge start time
-    start_soc_time: Optional[datetime] = None
+    start_soc_time: Optional[datetime] = Field(
+        default=None,
+        description=(
+            "The datetime that specifies the time after which the battery is"
+            "available for charging/discharging."
+        ),
+    )
 
-    # Capacity (Energie)
-    capacity: float = Field(gt=0, description="Battery capacity in Wh")
+    capacity: float = Field(
+        gt=0,
+        title="Capacity (Energy)",
+        description="The capacity of the battery in Wh.",
+    )
 
-    # Max. charge power
-    max_charge_power: float = Field(ge=0)
+    max_charge_power: float = Field(
+        ge=0,
+        title="Max. charge power",
+        description="The maximum power the battery can be charged with in W.",
+    )
     # Max. discharge power (0 if unidirectional charging)
-    max_discharge_power: float = Field(ge=0, default=0)
+    max_discharge_power: float = Field(
+        ge=0,
+        default=0,
+        title="The maximum discharge power of the battery",
+        description=(
+            "The maximum power the battery can be discharged with in W."
+            "This is optional. If it isn't supplied discharging the battery is"
+            "not allowed."
+        ),
+    )
 
     # Minimum charge power if the battery is charging
     min_charge_power: float = Field(ge=0, default=0)
@@ -81,29 +101,41 @@ class Battery(BaseModel):
     min_discharge_power: float = Field(ge=0, default=0)
 
     # Wirkungsgrad Laden
-    charge_efficiency: float = Field(ge=0, le=1, default=1)
-
-    # Validate that the discharge efficiency is not NaN
-    @field_validator("discharge_efficiency")
-    @classmethod
-    def discharge_efficiency_not_nan(cls, v: float):
-        if isnan(v):
-            log.warning("Discharge efficiency is NaN. Setting to 1")
-            return 1
-        return v
+    charge_efficiency: float = Field(
+        ge=0,
+        le=1,
+        default=1,
+        title="The efficiency of battery charging",
+        description="Efficiency of the charging process in percent.",
+    )
 
     # Wirkungsgrad Entladen
-    discharge_efficiency: float = Field(ge=0, le=1, default=1)
-
-    # Validate that the discharge efficiency is not NaN
-    @field_validator("charge_efficiency")
-    @classmethod
-    def charge_efficiency_not_nan(cls, v: float):
-        if isnan(v):
-            log.warning("Charge efficiency is NaN. Setting to 1")
-            return 1
-        return v
+    discharge_efficiency: float = Field(
+        ge=0,
+        le=1,
+        default=1,
+        title="Efficiency of battery discharging",
+        description="Efficiency of the discharge process in percent.",
+    )
 
     # Minimum/Maximum SoC at any time
-    min_soc: Optional[float] = Field(ge=0, le=1, default=0)
-    max_soc: Optional[float] = Field(ge=0, le=1, default=1)
+    min_soc: Optional[float] = Field(
+        ge=0,
+        le=1,
+        default=0,
+        title="Minimum State of Charge to not discharge below",
+        description=(
+            "Constraint the usable SoC range of the battery."
+            "Value is given in percent. This value is optional."
+        ),
+    )
+    max_soc: Optional[float] = Field(
+        ge=0,
+        le=1,
+        default=1,
+        title="Maximum State of Charge to charge to",
+        description=(
+            "Constraint the usable SoC range of the battery."
+            "Value is given in percent. This value is optional."
+        ),
+    )
