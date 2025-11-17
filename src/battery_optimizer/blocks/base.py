c412713b6ec4b71d814797ae48ac53bd0dc881fb
Carl Hauschke <hauschke@fzi.de>
2025-11-17 15:54:50 +0100

    add extra documentation

--- src/battery_optimizer/blocks/base.py
+++ src/battery_optimizer/blocks/base.py
@@ -12,16 +12,23 @@ class BaseBlock:
     """
     This is the template for each block used in the model.
 
-    Each block gets some common parameters, namely energy_source and 
+    Each block gets some common parameters, namely energy_source and
     energy_sink and price_source and price_sink.
     These can be adjusted in a concrete implementation. The energy limits
     are set to 0 by default and prices get a default value of 0 too.
+
+    Parameters
+    ----------
+    index : pyo.Set
+        The index of the model the component will be used in.
     """
     def __init__(self, index: pyo.Set):
         """
         Initialize a new block.
 
         Stores information of a component.
+        Overriding classes may add more parameters and variables that are
+        specific to the component.
 
         Parameters
         ----------
@@ -31,16 +38,35 @@ class BaseBlock:
         self.index = index
 
     def build_block(self) -> pyo.Block:
-        """Build the battery block"""
+        """
+        Build the block.
+
+        Create the pyomo block, add common parameters and variables for the
+        energy matrix and call the method to populate the block with
+        specific constraints and variables.
+
+        Returns
+        -------
+        pyo.Block
+            A Pyomo block representing the component.
+
+        Examples
+        --------
+        >>> self.model.my_blocks.add_component(
+        ...     name=name,
+        ...     val=MyBlock(
+        ...         self.model.i
+        ...     ).build_block(),
+        ... )
+        """
         block = pyo.Block()
-        # DEFAULT
         # Source in Matrix
         block.energy_source = pyo.Var(self.index, bounds=(0, 0), initialize=0)
         block.price_source = pyo.Param(self.index, initialize=0, mutable=True)
         # Sink in matrix
         block.energy_sink = pyo.Var(self.index, bounds=(0, 0), initialize=0)
         block.price_sink = pyo.Param(self.index, initialize=0, mutable=True)
-        # DEFAULT
+
         block.energy_source.construct()
         block.price_source.construct()
         block.energy_sink.construct()
