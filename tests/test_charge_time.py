e4f2843b3962148e15ad06192bebaeb1f4b1b3fc
Carl Hauschke <hauschke@fzi.de>
2025-12-09 16:21:24 +0100

    add tolerance

--- tests/test_charge_time.py
+++ tests/test_charge_time.py
@@ -120,10 +120,24 @@ class TestChargeTime(unittest.TestCase):
         )
 
         # Assert power profiles
-        pd.testing.assert_frame_equal(result[0], buy_result, check_dtype=False)
         pd.testing.assert_frame_equal(
-            result[1], sell_result, check_dtype=False
+            result[0],
+            buy_result,
+            check_dtype=False,
+            rtol=1e-6,
+            atol=1e-8,
         )
         pd.testing.assert_frame_equal(
-            result[4], fixed_consumption_result, check_dtype=False
+            result[1],
+            sell_result,
+            check_dtype=False,
+            rtol=1e-6,
+            atol=1e-8,
+        )
+        pd.testing.assert_frame_equal(
+            result[4],
+            fixed_consumption_result,
+            check_dtype=False,
+            rtol=1e-6,
+            atol=1e-8,
         )
