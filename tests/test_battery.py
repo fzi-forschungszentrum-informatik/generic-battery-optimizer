d76bdf4a8cd1b56527ab77303336c9ae31e1bdc8
Carl Hauschke <hauschke@fzi.de>
2025-12-15 09:50:11 +0100

    add tests for ev with charging end after profile time end

--- tests/test_battery.py
+++ tests/test_battery.py
@@ -11,6 +11,10 @@ import pytest
 import pandas as pd
 from datetime import datetime
 from battery_optimizer.export import Exporter
+from battery_optimizer.helpers.model_setup import (
+    generate_common_time_series,
+    reindex_profile,
+)
 from battery_optimizer.model import Model
 from battery_optimizer.profiles.battery import Battery
 from battery_optimizer import optimize
@@ -347,3 +351,329 @@ class TestMinMaxSoc:
                 min_soc=min_soc,
                 max_soc=max_soc,
             )
+
+
+class TestDeprecatedEVFunctionality:
+    """
+    Test the deprecated EV functionality in the Battery profile.
+
+    The EV functionality has been moved to the EV class in profiles.ev and
+    model.add_ev(). These tests ensure that the deprecated functionality still
+    works as expected until it is removed in version 5.0.0.
+    """
+
+    def test_charge_end_past_last_timestamp(self):
+        """
+        Test the charging of an EV where charge_end_time is after last ts.
+
+        Ensure that the end soc will be reached at the last timestamp when
+        charge_end_time is after the last timestamp from the profile and
+        the generate_common_time_series is used.
+        """
+        time_series = [pd.to_datetime("2025-12-11T13:58:05.129253Z")]
+        time_series.extend(
+            pd.date_range(
+                start="2025-12-11T14:00:00Z",
+                end="2025-12-12T01:45:00Z",
+                freq="15min",
+            )
+        )
+
+        pv_power = {
+            time: price
+            for time, price in zip(
+                time_series,
+                [
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    -10536,
+                    -15668,
+                    -16781,
+                    -6810,
+                    0,
+                    0,
+                    -23311,
+                    -35173,
+                    -2398,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    -21550,
+                    -28200,
+                    -17261,
+                    0,
+                    -12326,
+                    -724,
+                    -3187,
+                    0,
+                    -30316,
+                    0,
+                    0,
+                    0,
+                    0,
+                    -157,
+                    0,
+                    0,
+                    -10097,
+                    -752,
+                    0,
+                    -3229,
+                    0,
+                    0,
+                ],
+            )
+        }
+        pv_price = {time: 0.0 for time in time_series}
+
+        grid_power = {time: 1000000 for time in time_series}
+        grid_price = {
+            time: price
+            for time, price in zip(
+                time_series,
+                [
+                    33.717459999999996,
+                    34.18275,
+                    34.18275,
+                    34.18275,
+                    34.18275,
+                    34.774179999999994,
+                    34.774179999999994,
+                    34.774179999999994,
+                    34.774179999999994,
+                    39.93283,
+                    39.93283,
+                    39.93283,
+                    39.93283,
+                    39.703160000000004,
+                    39.703160000000004,
+                    39.703160000000004,
+                    39.703160000000004,
+                    39.696020000000004,
+                    39.696020000000004,
+                    39.696020000000004,
+                    39.696020000000004,
+                    39.659130000000005,
+                    39.659130000000005,
+                    39.659130000000005,
+                    39.659130000000005,
+                    33.278349999999996,
+                    33.278349999999996,
+                    33.278349999999996,
+                    33.278349999999996,
+                    31.9753,
+                    31.9753,
+                    31.9753,
+                    31.9753,
+                    30.258129999999994,
+                    30.258129999999994,
+                    30.258129999999994,
+                    30.258129999999994,
+                    30.901919999999997,
+                    30.901919999999997,
+                    30.901919999999997,
+                    30.901919999999997,
+                    21.715119999999995,
+                    21.715119999999995,
+                    21.715119999999995,
+                    21.715119999999995,
+                    21.713929999999998,
+                    21.713929999999998,
+                    21.713929999999998,
+                    21.713929999999998,
+                ],
+            )
+        }
+
+        sell_power = {
+            time: power
+            for time, power in zip(
+                time_series,
+                [
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    -10536,
+                    -15668,
+                    -16781,
+                    -6810,
+                    0,
+                    0,
+                    -23311,
+                    -35173,
+                    -2398,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    -21550,
+                    -28200,
+                    -17261,
+                    0,
+                    -12326,
+                    -724,
+                    -3187,
+                    0,
+                    -30316,
+                    0,
+                    0,
+                    0,
+                    0,
+                    -157,
+                    0,
+                    0,
+                    -10097,
+                    -752,
+                    0,
+                    -3229,
+                    0,
+                    0,
+                ],
+            )
+        }
+        sell_price = {time: 8 for time in time_series}
+
+        fixed_consumption = {
+            time: power
+            for time, power in zip(
+                time_series,
+                [
+                    28380,
+                    27479,
+                    27597,
+                    27770,
+                    27307,
+                    27015,
+                    26607,
+                    26456,
+                    26729,
+                    26875,
+                    26556,
+                    25733,
+                    24815,
+                    23688,
+                    23238,
+                    23588,
+                    23397,
+                    22607,
+                    21736,
+                    21232,
+                    21204,
+                    20965,
+                    20343,
+                    19692,
+                    19422,
+                    19396,
+                    18891,
+                    20508,
+                    23095,
+                    21381,
+                    18398,
+                    17767,
+                    18052,
+                    18070,
+                    17591,
+                    17117,
+                    16855,
+                    16727,
+                    16786,
+                    16593,
+                    16263,
+                    16304,
+                    16257,
+                    16152,
+                    16268,
+                    16225,
+                    16112,
+                    16219,
+                    16136,
+                ],
+            )
+        }
+
+        battery = Battery.model_validate(
+            {
+                "name": "ev_battery",
+                "start_soc": 0.2,
+                "end_soc": 0.8,
+                "end_soc_time": "2025-12-12T01:58:05.129253Z",
+                "start_soc_time": "2025-12-11T13:58:05.129253Z",
+                "capacity": 77000,
+                "max_charge_power": 11000,
+                "max_discharge_power": 0,
+                "min_charge_power": 0,
+                "min_discharge_power": 0,
+                "charge_efficiency": 1,
+                "discharge_efficiency": 1,
+                "min_soc": 0,
+                "max_soc": 1,
+            }
+        )
+        with pytest.deprecated_call():
+            time_series = generate_common_time_series(
+                profiles=[
+                    pv_power,
+                    pv_price,
+                    grid_power,
+                    grid_price,
+                    sell_power,
+                    sell_price,
+                    fixed_consumption,
+                ],
+                batteries=[battery],
+            )
+
+        model = Model(time_series)
+        model.add_sell_profile(
+            "pv",
+            reindex_profile(pv_power, time_series),
+            reindex_profile(pv_price, time_series),
+        )
+        model.add_buy_profile(
+            "buy",
+            reindex_profile(grid_power, time_series),
+            reindex_profile(grid_price, time_series),
+        )
+        model.add_sell_profile(
+            "sell",
+            reindex_profile(sell_power, time_series),
+            reindex_profile(sell_price, time_series),
+        )
+        model.add_fixed_consumption(
+            "fixed_consumption",
+            reindex_profile(fixed_consumption, time_series),
+        )
+        with pytest.deprecated_call():
+            model.add_battery(battery)
+
+        model.add_energy_paths()
+        model.generate_objective()
+        Solver(find_solver()).solve(model.model)
+
+        df_export = Exporter(model).to_df()
+        assert df_export.to_battery_soc().iloc[-1][
+            "ev_battery"
+        ] == pytest.approx(0.8, 0.01)
